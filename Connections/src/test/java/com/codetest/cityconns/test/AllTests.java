8613d151d79a0ec63cac34e5a3a772d6e172d013
AmareswaraKathi <68030166+AmareswaraKathi@users.noreply.github.com>
2020-07-08 19:30:40 -0700

    Test Suite class to run all tests

---
+++ Connections/src/test/java/com/codetest/cityconns/test/AllTests.java
@@ -0,0 +1,11 @@
+package com.codetest.cityconns.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ConnectionsTest.class})
+public class AllTests {
+
+}
